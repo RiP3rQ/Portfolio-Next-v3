97cbb5d2c3a8b1832646b6223037ea3161c3458a
Rafał Pompa <rafalpompa2000@gmail.com>
2024-06-22 17:28:03 +0200

    feat: footer-section

---
+++ src/components/footer.tsx
@@ -0,0 +1,46 @@
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { socialMedia } from "@/data";
+import MagicButton from "@/components/ui/Aceternity/MagicButton";
+
+const Footer = () => {
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+      <div className="flex flex-col items-center">
+        <h1 className="font-bold text-4xl md:text-5xl text-center lg:max-w-[45vw]">
+          Ready to take <span className="text-[#CBACF9]">your</span> digital
+          presence to the next level?
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let&apos;s discuss how I can help you
+          achieve your goals.
+        </p>
+        <a href="mailto:contact@jsmastery.pro">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 RiP3rQ
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6 mt-1">
+          {socialMedia.map((info) => (
+            <div
+              key={info.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={info.img} alt="icons" width={20} height={20} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/data/index.ts
+++ src/data/index.ts
@@ -96,3 +96,18 @@ export const projects = [
     link: "/ui.apple.com",
   },
 ];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
