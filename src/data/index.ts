bad08c1ba1dc14f8e3d61797d8f70b6f4e4aa16c
Rafał Pompa <rafalpompa2000@gmail.com>
2024-06-29 19:11:13 +0200

    feat: icons

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,47 +1,12 @@
-export const projects = [
-  {
-    id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://www.riperq-codes.pro/",
-  },
-  {
-    id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://www.riperq-codes.pro/",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://www.riperq-codes.pro/",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://www.riperq-codes.pro/",
-  },
-];
-
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "/socials/git.svg",
     link: "https://github.com/RiP3rQ",
   },
   {
     id: 2,
-    img: "/link.svg",
+    img: "/socials/link.svg",
     link: "https://www.linkedin.com/in/rafalpompa00/",
   },
 ];
