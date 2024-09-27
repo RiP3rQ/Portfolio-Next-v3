7e3e11ed78a3627079f922c79ef73cb2bb8859c5
Rafał Pompa <rafalpompa2000@gmail.com>
2024-09-27 23:30:05 +0200

    feat: sentry reverted until bundled components

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,4 @@
-import {withSentryConfig} from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
-const config = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Document-Policy",
-            value: "js-profiling",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "sentry-trace, baggage",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
-export default withSentryConfig(config, {
-  silent: !process.env.CI,
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  widenClientFileUpload: true,
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-  transpileClientSDK: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-});
+export default nextConfig;
